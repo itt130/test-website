ffbeb4732aab35f9c0fc56cf19dabff3bd58876c
itt130 <tanyongyi@gmail.com>
2025-05-18 18:25:56 +0800

    3

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,10 +18,10 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://your-docusaurus-site.example.com',
-  url: `https://${organizationName}.${projectName}`,
+  url: `https://${organizationName}`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: `/${projectName}/`,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
