c4dd68ea2d256c84e52bfff16e6104265601f50c
Your Name <you@example.com>
2025-05-18 19:14:15 +0800

    Deploy website - based on 74a354222b47dc4703f5940e9f846c22841827d4

--- assets/js/acecf23e.2cec6652.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktest_website=self.webpackChunktest_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/test-website/blog","blogTitle":"Blog","authorsListPath":"/test-website/blog/authors"}')}}]);
